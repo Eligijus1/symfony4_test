568f054344d43d5003dc42aaf0724476ef5d7507
Eligijus Stugys <eligijus.stugys@nfq.lt>
2018-11-21 13:46:17 +0200

    import and export Statements

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,19 +6,16 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// var $ = require('jquery');
+import '../css/app.css';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 // loads the jquery package from node_modules
-let $ = require('jquery');
+import $ from 'jquery';
 
 // import the function from greet.js (the .js extension is optional)
 // ./ (or ../) means to look for a local file
-let greet = require('./greet');
+import greet from './greet';
 
 $(document).ready(function () {
     $('body').prepend('<h1>' + greet('jill') + '</h1>');
--- assets/js/greet.js
+++ assets/js/greet.js
@@ -1,5 +1,5 @@
 // assets/js/greet.js
-module.exports = function(name) {
+export default function(name) {
     return `Yo yo ${name} - welcome to Encore!`;
 };
 
