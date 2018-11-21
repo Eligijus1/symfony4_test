59e5a13dd9824384c0f45340355e8ec7b1bd95a9
Eligijus Stugys <eligijus.stugys@nfq.lt>
2018-11-21 13:07:17 +0200

    Define controller with routing.

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number")
+     *
+     * @return Response
+     * @throws \Exception
+     *
+     * NOTE: path to this controller is http://localhost:8000/lucky/number
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
