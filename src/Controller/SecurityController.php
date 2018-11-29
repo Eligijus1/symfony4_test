94813b0f3f5ad06164cbe52f24613bdb81fa49a7
Eligijus Stugys <eligijus.stugys@nfq.lt>
2018-11-29 16:59:23 +0200

    Implemented logout.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -21,4 +21,18 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+    /**
+     * This is the route the user can use to logout.
+     *
+     * But, this will never be executed. Symfony will intercept this first
+     * and handle the logout automatically. See logout in app/config/security.yml
+     *
+     * @Route("/logout", name="security_logout")
+     * @throws \Exception
+     */
+    public function logout()
+    {
+        throw new \Exception('Logout check action should never be reached!');
+    }
 }
